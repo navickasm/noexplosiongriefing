4c6c4cfd7b192bc85482c87c7412339ea019914a
Mack Navickas <mjnavickas@gmail.com>
2025-01-01 00:54:34 -0600

    Added item frame cancellation, renamed methods

--- src/main/java/dev/navickasm/noexplosiongriefing/Main.java
+++ src/main/java/dev/navickasm/noexplosiongriefing/Main.java
@@ -1,8 +1,10 @@
 package dev.navickasm.noexplosiongriefing;
 
+import org.bukkit.entity.Creeper;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -14,10 +16,16 @@ public final class Main extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onEntityExplode(EntityExplodeEvent event) {
+    public void clearCreeperExplosionBlockList(EntityExplodeEvent event) {
         if (event.getEntityType() == EntityType.CREEPER) {
             event.blockList().clear();
         }
     }
 
+    @EventHandler
+    public void cancelItemFrameDamage(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Creeper && event.getEntityType() == EntityType.ITEM_FRAME) {
+            event.setCancelled(true);
+        }
+    }
 }
